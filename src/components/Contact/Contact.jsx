29bd929e6f521375873e61d8d4c39079d26efeea
idilkel <96882056+idilkel@users.noreply.github.com>
2023-04-29 18:20:30 +0300

    contact form validation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,52 +2,130 @@ import { useState } from "react";
 import "./Contact.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import FormContainer from "./FormContainer";
 
 const Contact = () => {
   const [validated, setValidated] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    fullName: "",
+    message: "",
+    subject: "",
+  });
+  const [errors, setErrors] = useState({});
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // console.log(event.target.formFullName.value);
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+  const setField = (field, value) => {
+    setForm({ ...form, [field]: value });
+    if (!!errors) {
+      setErrors({ ...errors, [field]: null });
     }
-    setValidated(true);
-    let fullName = event.target.formFullName.value;
-    let email = event.target.formEmail.value;
-    let subject = event.target.formSubject.value;
-    let message = event.target.formMessage.value;
-    console.log(fullName + ", " + email + ", " + subject + ", " + message);
-    // console.log(form);
   };
 
+  const validateForm = () => {
+    const { email, fullName, message, subject } = form;
+    const newErrors = {};
+
+    if (!email || email === "" || !email.match(/@/) || !email.match(/\./)) {
+      newErrors.email = "Please enter a valid email";
+    }
+
+    if (!fullName || fullName === "" || fullName.length < 3) {
+      newErrors.fullName = "Please enter a full name";
+    }
+
+    if (!message || message === "" || message.length < 10) {
+      newErrors.message =
+        "Please enter a message with minimum of 10 characters";
+    }
+
+    if (!subject || subject === "") {
+      newErrors.subject = "Please enter a subject";
+    }
+
+    return newErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formErrors = validateForm();
+
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      console.log("form submitted");
+      console.log(form);
+      setForm({ email: "", fullName: "", message: "", subject: "" });
+    }
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   // console.log(event.target.formFullName.value);
+  //   const form = event.currentTarget;
+  //   if (form.checkValidity() === false) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   }
+  //   setValidated(true);
+  //   let fullName = event.target.formFullName.value;
+  //   let email = event.target.formEmail.value;
+  //   let subject = event.target.formSubject.value;
+  //   let message = event.target.formMessage.value;
+  //   console.log(fullName + ", " + email + ", " + subject + ", " + message);
+  //   // console.log(form);
+  // };
+
   return (
-    <div className="Contact">
+    <FormContainer className="Contact">
       <h1>Contact us</h1>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      {/*<Form noValidate validated={validated} onSubmit={handleSubmit}>*/}
+      <Form>
         <Form.Group className="mb-3" controlId="formFullName">
           <Form.Label>Full Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter full name" required />
+          <Form.Control
+            type="text"
+            placeholder="Enter full name"
+            value={form.fullName}
+            onChange={(e) => setField("fullName", e.target.value)}
+            isInvalid={!!errors.fullName}
+            required
+          />
           <Form.Control.Feedback type="invalid">
-            Please enter full name.
+            {/*Please enter full name.*/}
+            {errors.fullName}
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formSubject">
           <Form.Label>Subject</Form.Label>
-          <Form.Control type="text" placeholder="Enter subject" required />
+          <Form.Control
+            type="text"
+            placeholder="Enter subject"
+            value={form.subject}
+            onChange={(e) => setField("subject", e.target.value)}
+            isInvalid={!!errors.subject}
+            required
+          />
           <Form.Control.Feedback type="invalid">
-            Please enter a subject.
+            {/*Please enter a subject.*/}
+            {errors.subject}
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={form.email}
+            onChange={(e) => setField("email", e.target.value)}
+            isInvalid={!!errors.email}
+            required
+          />
           <Form.Control.Feedback type="invalid">
-            Please enter valid email.
+            {/* Please enter valid email.*/}
+            {errors.email}
           </Form.Control.Feedback>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
@@ -57,21 +135,31 @@ const Contact = () => {
         <Form.Group className="mb-3" controlId="formMessage">
           <Form.Label>Message</Form.Label>
           <Form.Control
+            as="textarea"
             type="textarea"
-            rows={5}
+            rows={3}
             placeholder="Enter message"
+            value={form.message}
+            onChange={(e) => setField("message", e.target.value)}
+            isInvalid={!!errors.message}
             required
           />
           <Form.Control.Feedback type="invalid">
-            Please enter a message.
+            {/* Please enter a message.*/}
+            {errors.message}
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button
+          onClick={handleSubmit}
+          variant="primary"
+          type="submit"
+          className="my-2"
+        >
           Submit
         </Button>
       </Form>
-    </div>
+    </FormContainer>
   );
 };
 
---
+++ src/components/Contact/FormContainer.js
@@ -0,0 +1,19 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const FormContainer = ({ children }) => {
+  return (
+    <Container>
+      {/*<Row className="justify-content-md-center">
+  <Col xs={12} md={6}>*/}
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default FormContainer;
