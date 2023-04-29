b036caeac5fe53109474120198008d3373b323cd
idilkel <96882056+idilkel@users.noreply.github.com>
2023-04-29 12:53:09 +0300

    form added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
 import "./Contact.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Contact = () => {
-  return <div className="Contact">Contact Us</div>;
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(event.target.formFullName.value);
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    let fullName = event.target.formFullName.value;
+    let email = event.target.formEmail.value;
+    let subject = event.target.formSubject.value;
+    let message = event.target.formMessage.value;
+    console.log(fullName + ", " + email + ", " + subject + ", " + message);
+    // console.log(form);
+  };
+
+  return (
+    <div className="Contact">
+      <h1>Contact us</h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formFullName">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter full name" required />
+          <Form.Control.Feedback type="invalid">
+            Please enter full name.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formSubject">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control type="text" placeholder="Enter subject" required />
+          <Form.Control.Feedback type="invalid">
+            Please enter a subject.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control.Feedback type="invalid">
+            Please enter valid email.
+          </Form.Control.Feedback>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            type="textarea"
+            rows={5}
+            placeholder="Enter message"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a message.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Contact;
