2ea1e88b6ab1e7f03b1106f44ee9a787c4ca02f2
idilkel <96882056+idilkel@users.noreply.github.com>
2023-05-01 14:00:09 +0300

    GitHub pages doesn't support BrowserRouter - fixed

--- src/index.js
+++ src/index.js
@@ -5,16 +5,30 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//GitHub pages doesn't support the tech used by the BrowserRouter
+//HashRouter and RouterProvider are required
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="/currency-exchange">
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  // <BrowserRouter basename="/currency-exchange">
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+  // </BrowserRouter>
 
   // </React.StrictMode>
 );
